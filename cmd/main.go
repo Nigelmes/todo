519b4f973febfbb5cba588ee3424cd3cfae4d9d3
Nigelms <almazchic@gmail.com>
2023-04-13 23:38:58 +0300

    service and repository

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"github.com/nigelmes/todo"
 	"github.com/nigelmes/todo/pkg/handler"
+	"github.com/nigelmes/todo/pkg/repository"
+	"github.com/nigelmes/todo/pkg/service"
 	"log"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	server := new(todo.Server)
 	if err := server.Run("8080", handlers.InitRoutes()); err != nil {
